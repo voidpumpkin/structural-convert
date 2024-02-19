454d0f6e663d64ac15cb16312cfb4f1034c6aced
Julius Lungys <32368314+voidpumpkin@users.noreply.github.com>
2024-02-19 18:48:20 +0200

    Remoce unused attribute

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use syn::DeriveInput;
 
 use crate::proc_macro::TokenStream;
 
-#[proc_macro_derive(StructuralConvert, attributes(convert, convert2))]
+#[proc_macro_derive(StructuralConvert, attributes(convert))]
 pub fn structural_convert(item: TokenStream) -> TokenStream {
     structural_convert::structural_convert_impl(parse_macro_input!(item as DeriveInput))
         .unwrap_or_else(syn::Error::into_compile_error)
